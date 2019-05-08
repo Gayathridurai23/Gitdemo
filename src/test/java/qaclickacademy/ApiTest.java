50278606d742ba6d3b526146d48a755f5ac557de
Gayathrid <gayathridurai23@gmail.com>
2019-05-08 16:37:24 +0530

    My First Commit

---
+++ src/test/java/qaclickacademy/ApiTest.java
@@ -0,0 +1,12 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class ApiTest {
+
+	@Test
+	public void login()
+	{
+		System.out.println("Login Success");
+	}
+}
---
+++ src/test/java/qaclickacademy/ProgramTest.java
@@ -0,0 +1,12 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class ProgramTest {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("Hi");
+	}
+}
